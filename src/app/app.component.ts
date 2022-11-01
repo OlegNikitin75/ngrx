2765af5accf134ee98f43723e513fd22bc0c04f9
OlegNikitin75 <nov075@mail.ru>
2022-11-01 15:41:56 +0300

    NGRX-3/feat add store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core'
+import { Store } from '@ngrx/store'
+import { clear, countSelector, decrement, increment } from './reducers/counter'
+import { map } from 'rxjs'
 
 @Component({
   selector: 'app-root',
@@ -6,25 +9,24 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  counter = 0
   updateAt?: number
+  count$ = this.store.select(countSelector)
+  cannotDecrement$ = this.count$.pipe(map(count => count <= 0))
 
-  get cannotDecrement(): boolean {
-    return this.counter <= 0
-  }
+  constructor(private store: Store) {}
 
   increment(): void {
     this.updateAt = Date.now()
-    this.counter++
+    this.store.dispatch(increment())
   }
 
   decrement(): void {
     this.updateAt = Date.now()
-    this.counter--
+    this.store.dispatch(decrement())
   }
 
   clear(): void {
     this.updateAt = Date.now()
-    this.counter = 0
+    this.store.dispatch(clear())
   }
 }
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,19 +1,15 @@
-import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
-} from '@ngrx/store';
-import { environment } from '../../environments/environment';
+import { ActionReducerMap, MetaReducer } from '@ngrx/store'
+import { environment } from '../../environments/environment'
+import { counterReducer, CounterState } from './counter'
 
 export interface State {
-
+  counter: CounterState
 }
 
 export const reducers: ActionReducerMap<State> = {
+  counter: counterReducer
+}
 
-};
-
-
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<State>[] = !environment.production
+  ? []
+  : []
